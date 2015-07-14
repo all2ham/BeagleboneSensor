e6acffdaab5ad0ef42e0581e3d79673fa761f9ac
Allan Hamzic <allanhamzic@gmail.com>
2015-07-14 19:19:07 -0400

    added frame for test code

---
+++ test_DAC_to_ADC.py
@@ -0,0 +1,38 @@
+# test_DAC_to_ADC
+# hardware
+# beaglbone black rev C
+# DAC: MCP4922
+# ADC: MCP3202
+# Allan Hamzic
+# July 15 2015
+
+# import
+from Adafruit.BBIO import SPI
+import Adafruit.GPIO as GPIO
+
+# init
+CS_dac = "P11"
+CS_adc = "P30"
+
+# DAC logic
+
+
+
+
+
+# ADC logic
+
+
+
+
+
+
+# test output [0.5V, 0,8V, 1.2V, 2.7V, 3.3V]
+
+
+
+
+
+
+
+# save input
